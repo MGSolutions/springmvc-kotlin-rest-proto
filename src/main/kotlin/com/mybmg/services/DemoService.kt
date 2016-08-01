1f24a4a83a96ef70c4a034734e3aa0f09f74649e
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2016-08-01 11:55:43 +0200

    Update Spring Boot to 1.4.0.RELEASE
    Simplify HelloWorldControllerTest

--- src/main/kotlin/com/mybmg/services/DemoService.kt
+++ src/main/kotlin/com/mybmg/services/DemoService.kt
@@ -3,12 +3,12 @@ package com.mybmg.services
 import org.springframework.stereotype.Service
 
 /**
- *
+ * Notice 'open' !!!
  */
 @Service
 open class DemoService {
 
-    fun doIt() {
+    open fun doIt() {
         println("In a service!!!")
     }
 }
--- src/test/kotlin/com/mybmg/controllers/HelloWorldControllerTest.kt
+++ src/test/kotlin/com/mybmg/controllers/HelloWorldControllerTest.kt
@@ -1,64 +1,35 @@
 package com.mybmg.controllers
 
-import com.mybmg.MyBmgRestInterfacesApplication
 import com.mybmg.services.DemoService
 import org.hamcrest.Matchers.equalTo
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.verify
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.restdocs.JUnitRestDocumentation
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get
+import org.springframework.restdocs.payload.PayloadDocumentation
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentationConfigurer
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get
-import org.springframework.restdocs.payload.PayloadDocumentation
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
-import org.springframework.test.context.web.WebAppConfiguration
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
-import org.springframework.web.context.WebApplicationContext
 
 
-@RunWith(SpringJUnit4ClassRunner::class)
-@SpringBootTest(classes = arrayOf(MyBmgRestInterfacesApplication::class))
+@RunWith(SpringRunner::class)
+@WebMvcTest(HelloWorldController::class, secure = false)
 @WebAppConfiguration
+@AutoConfigureRestDocs("target/generated-snippets")
 class HelloWorldControllerTest {
-
-    @get:Rule
-    val restDocumentation = JUnitRestDocumentation("target/generated-snippets")
-
-    @Autowired
-    private lateinit var applicationContext: WebApplicationContext
 
     @MockBean
     private lateinit var demoService: DemoService
 
     @Autowired
-    private lateinit var helloWorldController: HelloWorldController
-
     private lateinit var mockMvc: MockMvc
-
-    @Before
-    fun before() {
-        this.mockMvc = documentingMockMvc()
-    }
-
-    private fun documentingMockMvc(): MockMvc {
-        val documentationConfiguration: MockMvcRestDocumentationConfigurer = documentationConfiguration(this.restDocumentation)
-        val webAppContextSetup: DefaultMockMvcBuilder = MockMvcBuilders.webAppContextSetup(this.applicationContext)
-        val webAppContextWithDocs: DefaultMockMvcBuilder = webAppContextSetup.apply(documentationConfiguration)
-        return webAppContextWithDocs.build()!!
-    }
-
-    private fun standardMockMvc() = MockMvcBuilders.standaloneSetup(helloWorldController).build()
 
     @Test
     @Throws(Exception::class)
@@ -73,6 +44,6 @@ class HelloWorldControllerTest {
                         )
                 ))
 
-//        verify(demoService).doIt()
+        Mockito.verify(demoService).doIt()
     }
 }
